5263cde84ea8f49426bab67ab65b31b86a3e0681
Tim Clark <tim.clark@passivuk.com>
2025-02-06 12:42:12 +0000

    Bump the version to debug build process

--- sync.sh
+++ sync.sh
@@ -6,4 +6,4 @@ ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 TOKEN=$(aws codeartifact get-authorization-token --domain pst-cloud-services --domain-owner "${ACCOUNT_ID}" --query authorizationToken --output text)
 INDEX_URL=https://aws:${TOKEN}@pst-cloud-services-${ACCOUNT_ID}.d.codeartifact.eu-west-2.amazonaws.com/pypi/pst-cloud-services/simple/
 
-uv sync --refresh --all-packages --index "${INDEX_URL}"
+uv sync --verbose --refresh --all-packages --index "${INDEX_URL}"
