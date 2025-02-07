ef1649df8875ca66bc900396b21ddd2d9eb22d46
Tim Clark <tim.clark@passivuk.com>
2025-02-07 15:57:23 +0000

    Pass package name upto service super class

--- doughnut_service/src/doughnut_service/service.py
+++ doughnut_service/src/doughnut_service/service.py
@@ -1,9 +1,10 @@
 from constructs import Construct
 from pst_foundation_service.foundation import Service
 from pst_foundation_service.gateway import Gateway
+from pst_foundation_service.package import name
 
 
 class DoughnutService(Service):
     def __init__(self, scope: Construct, construct_id: str, gateway: Gateway):
-        super().__init__(scope, construct_id, "doughnut-service", gateway)
+        super().__init__(scope, construct_id, name(__package__), gateway)
         # TODO - add actual service resources
--- sync.sh
+++ sync.sh
@@ -6,4 +6,6 @@ ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 TOKEN=$(aws codeartifact get-authorization-token --domain pst-cloud-services --domain-owner "${ACCOUNT_ID}" --query authorizationToken --output text)
 INDEX_URL=https://aws:${TOKEN}@pst-cloud-services-${ACCOUNT_ID}.d.codeartifact.eu-west-2.amazonaws.com/pypi/pst-cloud-services/simple/
 
+echo $INDEX_URL
+
 uv sync --verbose --refresh --all-packages --index "${INDEX_URL}"
